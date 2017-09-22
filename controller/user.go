9ec163331c7e07760bc33d94d7f12a53f528eafa
Balthild Ires <ibalthild@gmail.com>
2017-09-23 02:34:50 +0800

    Split articles into different .md-card according to year

--- controller/user.go
+++ controller/user.go
@@ -1,8 +1,8 @@
 package controller
 
 import (
-	"../util"
 	"../db"
+	"../util"
 	"github.com/Automattic/go-gravatar"
 	"github.com/kataras/iris/context"
 )
@@ -14,10 +14,23 @@ func UserPage(ctx context.Context) {
 	avatar.Size = 240
 
 	var articles []db.Article
-	db.Conn().Model(&user).Related(&articles)
+	db.Conn().Model(&user).Order("created_at desc, id desc").Related(&articles)
+
+	articlesByYears := make(map[int][]db.Article)
+
+	for _, article := range articles {
+		articlesByYear, ok := articlesByYears[article.CreatedAt.Year()]
+
+		if !ok {
+			articlesByYear = make([]db.Article, 0)
+		}
+
+		articlesByYear = append(articlesByYear, article)
+		articlesByYears[article.CreatedAt.Year()] = articlesByYear
+	}
 
 	ctx.ViewData("user_avatar_url", avatar.GetURL())
 	ctx.ViewData("user", user)
-	ctx.ViewData("articles", articles)
+	ctx.ViewData("articles_by_year", articlesByYears)
 	ctx.View("user.html")
 }
